dde04699e432ca0a36ee5e69b67c7ca14aea8278
Guilherme-Wolff <95699454+Guilherme-Wolff@users.noreply.github.com>
2024-11-11 14:21:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,22 @@ const disk = require('diskusage');
 const path = require('path');
 const app = express();
 
+const diskPath = process.platform === 'win32' ? 'C:\\' : '/';
+
+        // Obtenha o uso do disco
+const { available, free, total } = await disk.check(diskPath);
+
+        // Converta os valores para GB para facilitar a leitura
+const totalGB = (total / (1024 ** 3)).toFixed(2);
+const freeGB = (free / (1024 ** 3)).toFixed(2);
+const availableGB = (available / (1024 ** 3)).toFixed(2);
+
 app.all('/', async (req, res) => {
     console.log("Just got a request!");
 
     try {
         // Defina o caminho para a unidade de disco. Use "/" no Linux/Mac e "C:\\" no Windows.
-        const diskPath = process.platform === 'win32' ? 'C:\\' : '/';
-
-        // Obtenha o uso do disco
-        const { available, free, total } = await disk.check(diskPath);
-
-        // Converta os valores para GB para facilitar a leitura
-        const totalGB = (total / (1024 ** 3)).toFixed(2);
-        const freeGB = (free / (1024 ** 3)).toFixed(2);
-        const availableGB = (available / (1024 ** 3)).toFixed(2);
+        
 
         // Crie uma resposta JSON com as informações do disco
         res.json({
